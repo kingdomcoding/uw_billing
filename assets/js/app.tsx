157290c1129fee83f0334fa21932d01555635cc9
KingdomCoder <kingdomcoding@gmail.com>
2026-03-06 13:50:15 +0000

    wire OverviewPage as index route, remove Navigate redirect

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
+import OverviewPage from "./pages/OverviewPage"
 import SetupPage from "./pages/SetupPage"
 import UsagePage from "./pages/UsagePage"
 import BillingPage from "./pages/BillingPage"
@@ -12,7 +13,7 @@ createRoot(container).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/trades" replace />} />
+        <Route index element={<OverviewPage />} />
         <Route path="settings" element={<SetupPage />} />
         <Route path="usage"   element={<UsagePage />} />
         <Route path="billing" element={<BillingPage />} />
